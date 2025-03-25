d8ef2fed19372de66bb370d83527944551bd5e48
Didier_Defrance <didier@d-defrance.fr>
2025-03-25 14:23:51 +0100

    s

--- tp/mars2025/debutAppliSpringSansSpringBoot/src/main/java/tp/appliSpring/core/entity/Compte.java
+++ tp/mars2025/debutAppliSpringSansSpringBoot/src/main/java/tp/appliSpring/core/entity/Compte.java
@@ -4,26 +4,20 @@ package tp.appliSpring.core.entity;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.NamedQuery;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-
+import jakarta.persistence.*;
 
 
 //@NamedQuery(name = "Compte.findWithOperations" , query="...")
+@Entity
 public class Compte {
 
-   
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long numero;
-    
+
+   @Column(length = 50)
     private String label;
-    
+
     private Double solde;
     
   
