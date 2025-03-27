d5c9519666209d9306fd8680884c12ea09a99f1d
Didier_Defrance <didier@d-defrance.fr>
2025-03-27 13:32:44 +0100

    s

--- tp/mars2025/debutAppliSpringWeb/src/test/java/tp/appliSpring/core/service/TestServiceCompte.java
+++ tp/mars2025/debutAppliSpringWeb/src/test/java/tp/appliSpring/core/service/TestServiceCompte.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import tp.appliSpring.AppliSpringApplication;
