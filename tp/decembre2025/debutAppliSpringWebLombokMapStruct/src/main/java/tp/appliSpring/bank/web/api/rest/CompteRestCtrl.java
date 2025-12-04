809965282b5ba5dcd08142d6d61615005f94ab43
didier defrance <didier@d-defrance.fr>
2025-12-04 09:43:52 +0100

    s

--- tp/decembre2025/debutAppliSpringWebLombokMapStruct/src/main/java/tp/appliSpring/bank/web/api/rest/CompteRestCtrl.java
+++ tp/decembre2025/debutAppliSpringWebLombokMapStruct/src/main/java/tp/appliSpring/bank/web/api/rest/CompteRestCtrl.java
@@ -1,7 +1,7 @@
 package tp.appliSpring.bank.web.api.rest;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,12 +9,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import tp.appliSpring.bank.core.model.Compte;
 import tp.appliSpring.bank.core.service.ServiceCompte;
@@ -43,7 +45,6 @@ public class CompteRestCtrl {
 		this.serviceCompte = serviceCompte;
 	}
 
-	
 	// Get By ID
 	// V1 avec DTO et V3 (avec automatisme ExceptionHandler)
 	// declencher en mode GET avec
@@ -54,7 +55,6 @@ public class CompteRestCtrl {
 		// NB: l'objet retourné sera automatiquement converti au format json
 	}
 
-
 	/*
 	 * //V2 avec ResponseEntity<?> mais sans ExceptionHandler
 	 * 
@@ -62,7 +62,7 @@ public class CompteRestCtrl {
 	 * 
 	 * @GetMapping("/{id}") ...
 	 */
-	
+
 //	@GetMapping("/{id}")
 //	public ResponseEntity<Compte> getCompteById(@PathVariable("id") long numeroCompte) {
 //		Optional<Compte> optionalCompte =  serviceCompte.findById(numeroCompte);
@@ -80,14 +80,13 @@ public class CompteRestCtrl {
 	// http://localhost:8181/appliSpring/rest/api-bank/v1/comptes?soldeMini=50
 
 	// http://localhost:8181/appliSpring/rest/api-bank/v1/comptes?numClient=1
-	
+
 	// http://localhost:8181/appliSpring/rest/api-bank/v1/comptes?soldeMini=50&critere2=val2&critere3=val3
 	@GetMapping()
-	public List<Compte> getComptesByCriteria(
-			@RequestParam(value = "soldeMini", required = false) Double soldeMini,
+	public List<Compte> getComptesByCriteria(@RequestParam(value = "soldeMini", required = false) Double soldeMini,
 			@RequestParam(value = "numClient", required = false) Long numClient) {
 		if (soldeMini != null)
-			return serviceCompte.searchWithMinimumBalance(soldeMini);	
+			return serviceCompte.searchWithMinimumBalance(soldeMini);
 		else if (numClient != null)
 			return serviceCompte.searchCustomerAccounts(numClient);
 		else
@@ -98,7 +97,20 @@ public class CompteRestCtrl {
 	// avec url = http://localhost:8181/appliSpring/rest/api-bank/v1/comptes
 	// avec dans la partie "body" de la requête
 	// { "numero" : null , "label" : "comptequiVaBien" , "solde" : 50.0 }
-	// ...
+	@PostMapping("")
+	public ResponseEntity<?> postXyz(/* @Valid */ @RequestBody Compte compte) {
+		Compte savedCompte = serviceCompte.create(compte); // avec id auto_incrémenté
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(savedCompte.getNumero()).toUri();
+		// return ResponseEntity.created(location).build();
+		// return 201/CREATED , no body but URI to find added obj
+		return ResponseEntity.created(location).body(savedCompte);
+		// return 201/CREATED with savedObj AND with URI to find added obj
+		/*
+		 * ou bien encore return ResponseEntity.ok()
+		 * .headers(responseHeadersWithLocation).body(savedCompte);
+		 */
+	}
 
 	// appelé en mode PUT
 	// avec url = http://localhost:8181/appliSpring/rest/api-bank/v1/comptes/1
